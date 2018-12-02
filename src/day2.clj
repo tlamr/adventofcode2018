995919868c47c95b73eb7618f512f715433408f2
Tomas Lamr <tomas.lamr@pricefx.eu>
2018-12-02 14:06:11 +0100

    day2

---
+++ src/day2.clj
@@ -0,0 +1,46 @@
+(ns day2)
+
+(def input "tqzvwnogbarflkpcbdewsmjhxi\ntqyvunogzarfckpcbdewsmzhci\ntqyvunojzarflkpcbdcwsmyhxi\ntqyvunogzarclkpcbdewmmjrxi\nhqyvunogzarflkpcbczwsmjhxi\ntqyvunogzarflppcudewsmjhei\ntqrvunogzarflkpcbpewsmjhdi\naqyvunogzarflkpcbdewsjjsxi\ntqyvtnogzarflkkcbdewymjhxi\ntcyeunogzarfkkpcbdewsmjhxi\ntqyvunozzarfvkpcbdewsqjhxi\ntkyvuwjgzarflkpcbdewsmjhxi\ntqevunogzarflkpnbdkwsmjhxi\ntqyvunogqarflkpcppewsmjhxi\ntqyvunsgzarflkpcbrewsmjhxk\ntqyvunogzarffkpdbnewsmjhxi\ntqyvunogvarflkpjbdewsojhxi\ntqyvgnogzarflkpfbdswsmjhxi\ntqyvunogzarfxkpcbtersmjhxi\ntqyvukhgzarflupcbdewsmjhxi\ntqyvdnoozyrflkpcbdewsmjhxi\ntqyvunogzorflkpcbdewsvjhxd\ntqyvunzqzarflkpcbdewxmjhxi\ntqykunogzarulkpcbdhwsmjhxi\ntqycdnogzarflkpcbdewsmjhxz\neqyvunogzarflkpcbdhwqmjhxi\ncqyvunogzaralkpcbdewsmjvxi\nvqyvunogzarflklcbeewsmjhxi\ntqyvunogzarffkpcqdewlmjhxi\neqyvunogzarflkpcbdejsmahxi\ntqyvunogjarflkocsdewsmjhxi\ntqzvunogzanflkpcbdewsmjhxg\ntqycunogzarflkpabdewsmkhxi\ntqyvunogzarnlkpcbdewimjaxi\ntqyvunogzarfzkpcbdepsyjhxi\ngqykunogzarelkpcbdewsmjhxi\ntqyvuwogzarflkpcbdrwsmjmxi\ntqdqunogzarflkpcbdewsvjhxi\ntqmvunbgzarflkpcbdewsvjhxi\ntqyvunogzarflkpcbheesmjhdi\ntqxvunogzarfxkpcbdewsmhhxi\ntqyvunogzarflkpabdjosmjhxi\ntqyvugogztrflkpybdewsmjhxi\ntqyvundgzarflkxcbdewsmjhmi\ntqyvunogzurfgkpcbdeksmjhxi\ntqyvunokzarfckpcbdewsojhxi\ntqyvufobzarflkpcldewsmjhxi\ntqyvunogznrflkncbdeusmjhxi\ntqyvuncgzarfxkpcmdewsmjhxi\noqyvunogzarflkpybdewbmjhxi\ntqyvjnogqarfmkpcbdewsmjhxi\ntqyvunogzacflkpcidewsmjhwi\ntqyvunogzarflkpcbqlwxmjhxi\ntqyvunogzarflkpnbhewsvjhxi\nvqyiunogzarflkpcbdewsmjhqi\ntbyvuncgzarfljpcbdewsmjhxi\ntqylunogzarflkpcldexsmjhxi\ntqyvulogzarflktcbdewsxjhxi\ntqyvmnlgzarflkpcbxewsmjhxi\ntqyvunogzartlkpcbdewsmihxp\nnqyvunogzarflkpcbdewsmnwxi\ntqyvunogzarflkpczdewsmjcxj\ntzyvunogzariwkpcbdewsmjhxi\ntqyiufogzarflipcbdewsmjhxi\noqyvunogzasflkpcbdewsmjhxv\ntqyvunoqmarflkpcwdewsmjhxi\ntqrvunogzarflkpqbdewnmjhxi\ntqyvunogzarmlkocbdewsmjhri\ntqyvunogzakflkpcbveasmjhxi\ntqyvunorearflkpcbdewsmfhxi\ntqynrnogzarflkpcbdewsmjhxp\ntiyvueogzaeflkpcbdewsmjhxi\ntqyrunogzarflkpccdewbmjhxi\ntqtvunogzarflkpcbdewbnjhxi\ntqyvfnogzarflhpcbdewsmjqxi\ntqyjunoazarflkpcbdewssjhxi\ntqyvunxizarflkpcbdewsmjnxi\ntqyvunogzarfhkpobdewsmjhai\ntqyaunogzanflkpcbxewsmjhxi\ntqyvunogzarflkpsbuewsmjmxi\ntqyvunogzarzlkwybdewsmjhxi\ntqyvunogzarflkpibdawsmhhxi\ntqyvunogzarflkycbdewamjwxi\ntqyvunourarflkpcbdewsujhxi\ntqyvnnogzirflkpcbdewsdjhxi\ntayvunogzauflkpcqdewsmjhxi\ntqyvunobzfrflkscbdewsmjhxi\ntqygvnogzarflkpcbdewsmjnxi\noqyvunogzarflkpcbdewsmjsgi\ntqyvunokzarflkpcbdewshjhii\ntqyvunobzarflkvcbdewskjhxi\naqyvunogvarflkpcbdlwsmjhxi\ntqyvunogzmrrlbpcbdewsmjhxi\ntqyvunggzaqolkpcbdewsmjhxi\ntqyvunogqarflkpybdewsmjaxi\ntqyvunogzxrflkpcxsewsmjhxi\nzqyvunogzarflppcbdewsmjhni\ntqvvunogzakslkpcbdewsmjhxi\ntqyrunogzarzlkpcbdewsmjtxi\ntqyvhnogzarfxkpcbdewqmjhxi\ntqyvunogzarflkecbdewgdjhxi\ntqyvuwogzerfhkpcbdewsmjhxi\ntqmvunogzarflkpcbddwsmdhxi\ntqyvunogzarflcqcbdewsmihxi\ntqyvunogzarvlkpcbdewsmjmxd\ntqyvknogzarfllncbdewsmjhxi\ntqyvunogzarflbpcbdrwsajhxi\ntqyvunogzarfukpcbddwsmjhii\ntqyvuvojzahflkpcbdewsmjhxi\ntqyvunogzarfchpcbdeqsmjhxi\nwqivueogzarflkpcbdewsmjhxi\ntqyvunogzwrflkpcbdewstjhxz\ntqyvunogzarfloscbdewsmjhxf\ntqfbuiogzarflkpcbdewsmjhxi\ntqyvfuogzarflkpcxdewsmjhxi\ntqyvunogzarflkpcpdewsmgqxi\ntqyvunogzdrflkpcbdewsmqhci\ntqyvunogzartlkpcbdewsmjpxj\ntqyfunogzarfwkpcbdewsmwhxi\ntqyvuntgzarflkjcmdewsmjhxi\ntqyvunqfzarflkpckdewsmjhxi\nnqyvunogznrflkpcbiewsmjhxi\ntqymunobzarflkccbdewsmjhxi\ntqyvunogzaoflkprbdewzmjhxi\ntqyvunogzaiflkpcvdewbmjhxi\ntqwvunogzarfzkpcbdewsmjhxx\ntxhvunogzarflkpcbdewsijhxi\ntqyeunogkarflkicbdewsmjhxi\ntqylunogzarylkpcbdewsmkhxi\ntqyvriogzarflkpcbdewsmohxi\ntqyvunogzsrflkpcbdeasijhxi\ntqyvunogzarflkpcbfewcmjhxh\ntqyvunoozvrflkpcbdewimjhxi\ntqyvunogqayflkpcidewsmjhxi\ntqyounogzarflkpccdewsmjhxg\ntqgvunogsarflkpcbdewqmjhxi\ntqevunogzarflkpcbmewsmjhpi\ntqivunogzarflkgcbdewqmjhxi\ntqyuunogzlrflkgcbdewsmjhxi\nxqyvbnogznrflkpcbdewsmjhxi\ntqyvunogzarfjkpebdewsmnhxi\ntqyvvnogzarfskpcxdewsmjhxi\nthovunogzarflkpcbdewsmjhvi\ntqyvunugzarflkpcpdewsmjrxi\ntcyvvnogzarflkdcbdewsmjhxi\ntqdfunogzarflkpbbdewsmjhxi\ntqyvunogzarflkpcbdnwsejzxi\ntqyvunivkarflkpcbdewsmjhxi\ntqyvunogzawflopcbdewsmjhmi\ntqyvunogzarflkpcbdkwsdjqxi\ntqyvunodzarflkpcbdewlmjhei\noqyvunoozarflkpcbdemsmjhxi\ntiyvunogzarjlkpcbdewfmjhxi\ntqrvunogearflkpcbdewsojhxi\ntqyvunkgzarflkpcbdcwtmjhxi\ntqmvunogzarflkpcbdpwsmjtxi\ntqyvunogzarflkpcydeptmjhxi\ntqyvunkglarflkpcbdfwsmjhxi\ntqyaunogzarflkpcbzeesmjhxi\ntqyvunogzarrlkpcbdkwsmjhui\ntpyvunogzarflkqcbdewsmjhxd\ntvyvunogzarfkkpcbdewsajhxi\ngqynunogzarflepcbdewsmjhxi\nzqvvunogzarflkpcbdexsmjhxi\ntqyyunogzawflkpcbdewsmjhxw\ntfyvunogzarflkpcbdewomjrxi\ntqyeunogzvrflrpcbdewsmjhxi\nnqyvunogzarftkpabdewsmjhxi\ntzyvunogzariwkpcbdewmmjhxi\ntiyvunogzarflkpcbbewsmjhxa\ntqydujoyzarflkpcbdewsmjhxi\ntqyvunpgzarflkpcbdeysmjhwi\ntqyvunogvarllkpcbdewsmshxi\ntqyvunogzbrvlkpcbdewsmjhxp\ntcyvueogzarflkacbdewsmjhxi\ntqyvunogzrhflkpcbdetsmjhxi\ntqavunogzrdflkpcbdewsmjhxi\ntqyvunogzjrflkpcbdewstjhki\ntqyqunolzarflkpcbdewbmjhxi\ntqyvunogzarflkqczdgwsmjhxi\ntqyvunogzqrfrkpcbrewsmjhxi\ntqyvcnogzhrflkacbdewsmjhxi\ntqyvunogzarflkpcbdewsmdhtk\ntqyvunoggarftkpcbjewsmjhxi\ntvyvunogzarfkkpcpdewsmjhxi\ntqyvunogzawflkpcndedsmjhxi\ntqyvunogzrrflkpcbdemsmmhxi\ntqyvunogzarclkpcbrpwsmjhxi\ntryvunogztrflkpcbbewsmjhxi\ncqyvundgzarflkpcbdewgmjhxi\ntqyvunogzarflktcbkewsmjqxi\ntqyvjuogzarflkpcadewsmjhxi\ntqyvunogzyrflkpcbbxwsmjhxi\nttyvunogzarflkpcbdewsnmhxi\ntqyvunogzarflkpcbdeqsmlhki\nfqyvugogzarflapcbdewsmjhxi\ntqyvunogzartlkppbdewszjhxi\ntqyvunfgztrflzpcbdewsmjhxi\ntqyvunogmazflkpcbdewsmjhki\ntqyvunzdzarflkpcbdewsmjhvi\ntqyvunogzarflkpqbzewsujhxi\ntqyvunogzarzlkpcbeewymjhxi\ntqyounogzarflkpcbdewsnwhxi\ntqysunogsaiflkpcbdewsmjhxi\ntqdvunogdarflkpcboewsmjhxi\nteyvunogzarflkscbdfwsmjhxi\ntqyvunoazarflkpcbdvwsmjhpi\ntqyvunooearflkpcbdewrmjhxi\ntqyvunoszarflnrcbdewsmjhxi\ntqyvunogzalflkpcblewsjjhxi\nuqlvunkgzarflkpcbdewsmjhxi\ntqyvunobzarflkpcidewsmjhvi\ntnyvunogzarflkpcbdnwamjhxi\ntqyoudogzarflkpcbdgwsmjhxi\ntqyvunoggarflkpcbmewsmwhxi\ntqykunogzazflkpcbddwsmjhxi\ntfyvunogzarflkpcbdewsmjhgo\ntqyvunogztrflkpcbdewoojhxi\ntqyvunogzarflkpcbdewbmjoni\ntmyvunogzalflkpabdewsmjhxi\ntqyvunogzarflkpbbvewqmjhxi\ntqyvunofzarflkpcwdexsmjhxi\ntayvunogzasflkpcbdewsmhhxi\ntqyvlnogzarflkpcbdewsmjwxd\ntvyvunogzarflkpcbdewhmjrxi\ntqyvunogzarplkpcsdewsmihxi\ntqyvunogzarplklcbdewsmjtxi\nrqbvunogzarhlkpcbdewsmjhxi\ntqyvuxogzarftkpcbdewsmthxi\ntqtvunogzarfikpczdewsmjhxi\ntqyvunwgzarflkpcbdewsmjxxk\ntqygunogzarzlkpnbdewsmjhxi\ntqyvunogzarjlkpcbdbwswjhxi\ntqyvunogzalfnkpcbdewsmjhxf\ntqyucuogzarflkpcbdewsmjhxi\ntzyvunogzvrflkpcbdewsmjaxi\ntjyvunlgzarflkpcbdewgmjhxi\ntqyvcnogzarklkpcbdewsmjhfi\ntqyvunogzaaflkpsbaewsmjhxi\ntsyvunogzarflkpctdewsmbhxi\ntqyeunbgzarflkpcbdewrmjhxi\ntqyvunogzcrflqpcbdeismjhxi\neqylunogzarflkpcbdewsmjhxy\ntqyvundgzarflkpcbdewmmnhxi\ntzyvunogzarflkpcndewsmjhxb\ntkyvunogzxrflkpcbdewfmjhxi\ntqyvunogzarflkxcbdzwsmjfxi\nkqavunogzarflkycbdewsmjhxi\ntqyvunogzarjlkpcbdxwkmjhxi\ntqyvinogzarfqkpcbdewsmjpxi")
+
+;; part-one fns
+(defn parse-it [x]
+  (->> (clojure.string/split x #"\n")))
+
+(defn freqs [input]
+  (->> (parse-it input)
+    (map (fn [w]
+           (->> w
+             (frequencies)
+             (vals)
+             (apply hash-set))))))
+
+(defn find-containing [x input]
+  (filter (fn [xs]
+            (contains? xs x)) (freqs input)))
+
+;; part one
+(*
+  (count (find-containing 2 input))
+  (count (find-containing 3 input)))
+
+;; part two fns
+(defn filter-same [xs ys]
+  (filter some? (map (fn [[x y]]
+                       (if (= x y)
+                         x
+                         nil)) (map vector xs ys))))
+
+(defn differs-by [xs ys]
+  (let [fs (filter-same xs ys)]
+    [(- (count xs) (count fs)) fs]))
+
+(defn combinations [input]
+  (filter #(apply not= %) (partition 2 (flatten (for [x input]
+                                                  (map #(vector x %) input))))))
+
+;; part two
+(let [[[diff xs]] (take 1 (sort (fn [[x xs] [y ys]]
+                                  (compare x y)) (map #(apply differs-by %) (combinations (parse-it input)))))]
+  (clojure.string/join (map str xs)))
+
+
